775d58eabd01fdf70f91214bc6a406c88f1e82ab
Juan Francisco Carrion Molina <jcarrion@student.42madrid.com>
2021-08-17 13:34:26 +0200

    Added test for C Piscine C 03 00

---
+++ C Piscine C 03/ex00/main_test.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strcmp(char *s1, char *s2);
+
+void	heading(char *cursus, char *project, char *exercise)
+{
+	printf("\033[0;34m");
+	printf("   __  ___\n  /. |(__ \\    42 Testing by Juan Carrión\n (_  _)/ _/    %s %s\n   (_)(____)   %s\n\n\n", cursus, project, exercise);
+	printf("\033[0m");
+}
+
+void	test_ft_strcmp(int number, char *s1, char *s2)
+{
+	int	expected;
+	int	obtained;
+
+	printf("# Test %d\n\n", number);
+
+	expected = strcmp(s1, s2);
+	obtained = ft_strcmp(s1, s2);
+
+	printf("\033[0;33m");
+	printf("String 1: %s\n", s1);
+	printf("String 2: %s\n", s2);
+	printf("\033[0m");
+	printf("Expected return: %d\n", expected);
+	printf("Obtained return: %d\n", obtained);
+
+	if (obtained == expected)
+	{
+		printf("\033[0;32m");
+		printf("Test OK\n\n");
+		printf("\033[0m");
+	}
+	else
+	{
+		printf("\033[0;31m");
+		printf("Test failed\n\n");
+		printf("\033[0m");
+	}
+}
+
+int main(void)
+{
+	heading("C Piscine", "C 03", "00 - ft_strcmp");
+
+	test_ft_strcmp(1, "abcddddh", "abcdddd");
+
+	test_ft_strcmp(2, "abcdddd", "abcdddd");
+
+	test_ft_strcmp(3, "abcdddd", "abcd");
+}
