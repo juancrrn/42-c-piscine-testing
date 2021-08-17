bf9adb62e371bd80cd2587032cd3e97e54b08af3
Juan Francisco Carrion Molina <jcarrion@student.42madrid.com>
2021-08-17 16:17:43 +0200

    Added tests for C Piscine C 05 00, 01 and 02

---
+++ C Piscine C 05/ex01/main_test.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_recursive_factorial(int nb);
+
+void	heading(char *cursus, char *project, char *exercise)
+{
+	printf("\033[0;34m");
+	printf("   __  ___\n  /. |(__ \\    42 Testing by Juan Carrión\n (_  _)/ _/    %s %s\n   (_)(____)   %s\n\n\n", cursus, project, exercise);
+	printf("\033[0m");
+}
+
+void	test_ft_recursive_factorial(int number, int input, int expected)
+{
+	printf("# Test %d\n\n", number);
+
+	int obtained = ft_recursive_factorial(input);
+
+	printf("\033[0;33m");
+	printf("input: %d\n", input);
+	printf("\033[0m");
+	printf("Expected output: %d\n", expected);
+	printf("Obtained output: %d\n", obtained);
+
+	if (obtained == expected)
+	{
+		printf("\033[0;32m");
+		printf("Test OK\n\n");
+		printf("\033[0m");
+	}
+	else
+	{
+		printf("\033[0;31m");
+		printf("Test failed\n\n");
+		printf("\033[0m");
+	}
+}
+
+int main()
+{
+	heading("C Piscine", "C 05", "01 - test_ft_recursive_factorial");
+
+	test_ft_recursive_factorial(1, -1, 0);
+
+	test_ft_recursive_factorial(2, 0, 1);
+
+	test_ft_recursive_factorial(3, 1, 1);
+
+	test_ft_recursive_factorial(4, 2, 2);
+
+	test_ft_recursive_factorial(5, 3, 6);
+
+	test_ft_recursive_factorial(5, 10, 3628800);
+
+	test_ft_recursive_factorial(5, 17, -288522240);
+}
---
+++ C Piscine C 05/ex02/main_test.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_iterative_power(int nb, int power);
+
+void	heading(char *cursus, char *project, char *exercise)
+{
+	printf("\033[0;34m");
+	printf("   __  ___\n  /. |(__ \\    42 Testing by Juan Carrión\n (_  _)/ _/    %s %s\n   (_)(____)   %s\n\n\n", cursus, project, exercise);
+	printf("\033[0m");
+}
+
+void	test_ft_iterative_power(int number, int nb, int power, int expected)
+{
+	printf("# Test %d\n\n", number);
+
+	int obtained = ft_iterative_power(nb, power);
+
+	printf("\033[0;33m");
+	printf("nb   : %d\n", nb);
+	printf("power: %d\n", power);
+	printf("\033[0m");
+	printf("Expected output: %d\n", expected);
+	printf("Obtained output: %d\n", obtained);
+
+	if (obtained == expected)
+	{
+		printf("\033[0;32m");
+		printf("Test OK\n\n");
+		printf("\033[0m");
+	}
+	else
+	{
+		printf("\033[0;31m");
+		printf("Test failed\n\n");
+		printf("\033[0m");
+	}
+}
+
+int main()
+{
+	heading("C Piscine", "C 05", "02 - ft_iterative_power");
+
+	test_ft_iterative_power(1, 0, -1, 0);
+
+	test_ft_iterative_power(2, 0, 0, 1);
+
+	test_ft_iterative_power(3, 1, 0, 1);
+
+	test_ft_iterative_power(4, 1, 1, 1);
+
+	test_ft_iterative_power(5, 2, 0, 1);
+
+	test_ft_iterative_power(5, 2, 2, 4);
+
+	test_ft_iterative_power(5, 5, 2, 25);
+
+	test_ft_iterative_power(5, 5, 5, 3125);
+
+	test_ft_iterative_power(5, 2, 32, 0);
+}
