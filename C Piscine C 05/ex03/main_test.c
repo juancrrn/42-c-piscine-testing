6fb23048c6ac2bc82038c6d4a6c76f752edfcf42
Juan Francisco Carrion Molina <jcarrion@student.42madrid.com>
2021-08-18 18:49:37 +0200

    Updated tests

--- C Piscine C 05/ex03/main_test.c
+++ C Piscine C 05/ex03/main_test.c
@@ -41,19 +41,43 @@ int main()
 {
 	heading("C Piscine", "C 05", "03 - ft_recursive_power");
 
-	test_ft_recursive_power(1, 0, 0, 1);
+	test_ft_recursive_power(1, 0, -1, 0);
 
-	test_ft_recursive_power(2, 1, 0, 1);
+	test_ft_recursive_power(2, -1, 0, 1);
 
-	test_ft_recursive_power(3, 1, 1, 1);
+	test_ft_recursive_power(3, -1, -1, 0);
 
-	test_ft_recursive_power(4, 2, 0, 1);
+	test_ft_recursive_power(4, -2, 5, -32);
 
-	test_ft_recursive_power(5, 2, 2, 4);
+	test_ft_recursive_power(5, 5, -2, 0);
 
-	test_ft_recursive_power(6, 5, 2, 25);
+	test_ft_recursive_power(6, 0, 0, 1);
 
-	test_ft_recursive_power(7, 5, 5, 3125);
+	test_ft_recursive_power(7, 1, 0, 1);
 
-	test_ft_recursive_power(8, 2, 32, 0);
+	test_ft_recursive_power(8, 1, 1, 1);
+
+	test_ft_recursive_power(9, 2, 0, 1);
+
+	test_ft_recursive_power(10, 2, 2, 4);
+
+	test_ft_recursive_power(11, 5, 2, 25);
+
+	test_ft_recursive_power(12, 5, 5, 3125);
+
+	test_ft_recursive_power(13, 2, 32, 0);
+
+	test_ft_recursive_power(14, -3, -1, 0);
+	test_ft_recursive_power(15, -3, 0, 1);
+	test_ft_recursive_power(16, -3, 1, -3);
+	test_ft_recursive_power(17, -3, 2, 9);
+	test_ft_recursive_power(18, -3, 3, -27);
+	test_ft_recursive_power(19, 0, -1, 0);
+	test_ft_recursive_power(20, 0, 0, 1);
+	test_ft_recursive_power(21, 0, 2, 0);
+	test_ft_recursive_power(22, 4, -1, 0);
+	test_ft_recursive_power(23, 4, 0, 1);
+	test_ft_recursive_power(24, 4, 1, 4);
+	test_ft_recursive_power(25, 4, 2, 16);
+	test_ft_recursive_power(26, 4, 3, 64);
 }
